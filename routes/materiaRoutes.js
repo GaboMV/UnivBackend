2ca56adcad24b8b04cede62eaff648528d5fe03b
unknown <gabo171999@gmail.com>
2026-01-19 20:53:52 -0400

    Arreglando horarios

--- routes/materiaRoutes.js
+++ routes/materiaRoutes.js
@@ -11,5 +11,5 @@ module.exports = (materiaController, authMiddleware) => {
 
     router.get('/paralelos/:idMateria/:idEstudiante/:idSemestreActual', authMiddleware, materiaController.getParalelosDetalle);
 //
-router.get('/materias/estudiante/horario/:idEstudiante', authMiddleware, materiaController.getHorarioEstudiante);    return router;
+router.get('/estudiante/horario/:idEstudiante', authMiddleware, materiaController.getHorarioEstudiante);    return router;
 };
